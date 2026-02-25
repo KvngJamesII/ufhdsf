a305660020c7cffa600e0327064b705fe9e336d2
KvngJamesII <204447482+KvngJamesII@users.noreply.github.com>
2026-02-25 01:05:06 -0500

    Change PM2 exec_mode to fork for better stability

--- telegram-frontend/ecosystem.config.js
+++ telegram-frontend/ecosystem.config.js
@@ -3,6 +3,7 @@ module.exports = {
     name: 'luca-telegram-manager',
     script: './index.js',
     instances: 1,
+    exec_mode: 'fork',
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
